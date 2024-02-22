4485d38a37c1ca0fa23e5a1bd9bd302441e18439
Kristiyan <146897699+Krischo99@users.noreply.github.com>
2024-02-22 21:01:43 +0300

    add GUI for add words
    a GUI has been created from which you can add words that you can guess

--- Second Project/LanguageApp/src/guis/AddWordPanel.java
+++ Second Project/LanguageApp/src/guis/AddWordPanel.java
@@ -6,22 +6,45 @@ import java.awt.event.ActionListener;
 
 public class AddWordPanel extends JFrame {
 
-    public AddWordPanel(int width, int height, JMenuBar menuBar){
+    public AddWordPanel(int width, int height, JMenuBar menuBar) {
 
         addComponents(width, height, menuBar);
     }
 
-    private void addComponents(int width, int height, JMenuBar menuBar){
+    private void addComponents(int width, int height, JMenuBar menuBar) {
 
         // Setting up our JFrame
         setMandatoryFrameSettings(width, height);
-        getContentPane().setBackground(Color.BLUE);
+        getContentPane().setBackground(Color.RED);
 
         // Set already setup menu bar who come from our MainFrame
         setJMenuBar(menuBar);
         setActionsToMenuItems(menuBar);
 
-        
+        // Add text fields
+        JTextField word = new JTextField();
+        word.setFont(new Font("Word", Font.BOLD, 20));
+        word.setBounds(10, 80, 300, 40);
+
+        JTextField translated = new JTextField();
+        translated.setFont(new Font("Translated", Font.BOLD, 20));
+        translated.setBounds(10, 210, 300, 40);
+
+        JTextField emoji = new JTextField();
+        emoji.setFont(new Font("Emoji", Font.BOLD, 20));
+        emoji.setBounds(10, 340, 300, 40);
+
+        // Add all text fields to the frame
+        add(word);
+        add(translated);
+        add(emoji);
+
+        // Button who add this new word
+        JButton addWord = new JButton("Add word");
+        addWord.setBounds(560, 485, 200, 50);
+        addWord.setFocusPainted(false);
+        add(addWord);
+
         setVisible(true);
     }
 
@@ -35,7 +58,7 @@ public class AddWordPanel extends JFrame {
         }
     }
 
-    private void setActionsToMenuItems(JMenuBar menuBar){
+    private void setActionsToMenuItems(JMenuBar menuBar) {
 
         // Get first menu item
         JMenu firstMenu = menuBar.getMenu(0);
